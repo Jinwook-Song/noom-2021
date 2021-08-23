b636cf7980f99360937afcfec96ff4e1724e773f
jinwook-song <wlsdnr129@naver.com>
2021-08-23 20:34:26 +0900

    lecture 1.7 Nicknames part 1

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,16 +1,24 @@
 // Frontend
 
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#jsNickname");
+const messageForm = document.querySelector("#jsMessage");
 // Connect to the backend
 const fSocket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
 
 fSocket.addEventListener("open", () => {
   console.log("Connected to the Server ✅");
 });
 
 fSocket.addEventListener("message", (msg) => {
-  console.log("New message:", msg.data);
+  const li = document.createElement("li");
+  li.innerText = msg.data;
+  messageList.append(li);
 });
 
 fSocket.addEventListener("close", () => {
@@ -20,8 +28,15 @@ fSocket.addEventListener("close", () => {
 const handleSubmit = (event) => {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  fSocket.send(input.value);
+  fSocket.send(makeMessage("new_message", input.value));
   input.value = "";
 };
 
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  fSocket.send(makeMessage("nickname", input.value));
+};
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
