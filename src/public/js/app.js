c442be9879f0c6727823d1f7fe60c0c020f15f68
jinwook song <wlsdnr129@naver.com>
2022-02-20 18:07:32 +0900

    offer

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,12 +1,18 @@
-const socket = io();
+const fSocket = io();
 
 const myFace = document.getElementById('myFace');
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 
+const call = document.getElementById('call');
+
+// initialize
+call.hidden = true;
 let myStream;
 let muted = true;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -30,8 +36,6 @@ async function getMedia() {
     console.log(err);
   }
 }
-
-getMedia();
 
 function handleMuteClick() {
   myStream
@@ -61,3 +65,49 @@ function handleCameraClick() {
 
 muteBtn.addEventListener('click', handleMuteClick);
 cameraBtn.addEventListener('click', handleCameraClick);
+
+// Welcome Form (join a room)
+
+const welcome = document.getElementById('welcome');
+const welcomeForm = welcome.querySelector('form');
+
+async function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector('input');
+  fSocket.emit('join_room', input.value, startMedia);
+  roomName = input.value;
+  input.value = '';
+}
+
+welcomeForm.addEventListener('submit', handleWelcomeSubmit);
+
+// Socket Code
+
+fSocket.on('welcome', async () => {
+  // Peer A
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  // Send offer to Peer B
+  fSocket.emit('offer', offer, roomName);
+});
+
+// Peer B
+fSocket.on('offer', (offer) => {
+  console.log(offer, 'from peer A');
+});
+
+// RTC Code
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  // Peer Connection에  Audio, Video Track을 추가
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
--- src/server.js
+++ src/server.js
@@ -1,101 +1,28 @@
-// Backend
-
-import http from "http";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
-import express from "express";
+import http from 'http';
+import SocketIO from 'socket.io';
+import express from 'express';
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
-// http & ws on the save port (create ws server on http server)
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
+const wsServer = SocketIO(httpServer);
 
-instrument(wsServer, {
-  auth: false,
-});
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (bSocket) => {
-  bSocket["nickname"] = "Anonymous";
-  bSocket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-  bSocket.on("enter_room", (roomName, done) => {
+wsServer.on('connection', (bSocket) => {
+  bSocket.on('join_room', (roomName, done) => {
     bSocket.join(roomName);
     done();
-    bSocket.to(roomName).emit("welcome", bSocket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms());
+    bSocket.to(roomName).emit('welcome');
   });
-  bSocket.on("disconnecting", () => {
-    bSocket.rooms.forEach((room) =>
-      bSocket.to(room).emit("bye", bSocket.nickname, countRoom(room) - 1)
-    );
-  });
-  bSocket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  bSocket.on("new_message", (msg, roomName, done) => {
-    bSocket.to(roomName).emit("new_message", `${bSocket.nickname}: ${msg}`);
-    done();
-  });
-  bSocket.on("nickname", (nickname) => {
-    bSocket["nickname"] = nickname;
+  bSocket.on('offer', (offer, roomName) => {
+    bSocket.to(roomName).emit('offer', offer);
   });
 });
 
-// fake DB
-const sockets = [];
-
-// listening who connected
-// wss.on("connection", (bSocket) => {
-//   sockets.push(bSocket);
-//   bSocket["nickname"] = "Anonymous";
-//   console.log("Connected to the Browser ✅");
-//   bSocket.on("close", () => console.log("Disconnected from the Browser ❌"));
-//   bSocket.on("message", (msg) => {
-//     const parsedMsg = JSON.parse(msg);
-//     switch (parsedMsg.type) {
-//       case "new_message":
-//         sockets.forEach((aSocket) =>
-//           aSocket.send(`${bSocket.nickname}: ${parsedMsg.payload}`)
-//         );
-//         break;
-//       case "nickname":
-//         bSocket["nickname"] = parsedMsg.payload;
-//         break;
-//     }
-//   });
-// });
-
-const handleListen = () => console.log(`Listening on http://localhost:4000 ✅`);
-httpServer.listen(4000, handleListen);
+const handleListen = () => console.log(`✅ Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
